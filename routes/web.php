672fe524130fb187ae8098f60df2de643f4e042e
AngelGHZ <angelgtzhernandez@gmail.com>
2026-02-09 18:00:20 -0600

    chore: remove deploy route

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,4 @@ Route::get('/', fn() => redirect()->route('estados.index'));
 Route::get('/estados', [EstadoController::class, 'index'])->name('estados.index');
 Route::get('/estados/{id}/municipios', [EstadoController::class, 'municipios'])->name('estados.municipios');
 
-use Illuminate\Support\Facades\Artisan;
 
-Route::get('/__deploy', function () {
-    Artisan::call('migrate --force');
-    Artisan::call('copomex:sync-estados');
-    return nl2br(Artisan::output());
-});
